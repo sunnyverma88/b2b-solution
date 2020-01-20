d894bd5566d0e4ed82815f9f5eb180c93ee41aba
SHWETABH GAURAV <shwetabh.gaurav@gmail.com>
2020-01-20 17:08:39 -0500

    Product Defect

--- src/main/java/com/techieonthenet/entity/Product.java
+++ src/main/java/com/techieonthenet/entity/Product.java
@@ -42,7 +42,7 @@ public class Product extends Auditable implements Serializable {
     @JoinColumn(name = "category_id")
     private Category category;
 
-    @OneToMany(mappedBy = "product", cascade = CascadeType.DETACH)
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
     private List<Image> imgList;
 
 }
